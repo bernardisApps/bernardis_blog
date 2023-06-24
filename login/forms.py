eb5d669ec553359f6e23dcd7a34a24900a9e3ee5
bernardisApps <matias.bernardis@hotmail.com>
2023-06-24 08:44:33 -0300

    seguimos con las modificaciones de templates

--- login/forms.py
+++ login/forms.py
@@ -23,8 +23,8 @@ class RegistrationForm(UserCreationForm):
 
 class loginForm(forms.Form):
 
-    username = forms.CharField(max_length=50)
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
     
     class Meta:
         model = User

aef8eebe6135119fb0ba91075a899b32cc3d35c4
bernardisApps <matias.bernardis@hotmail.com>
2023-06-25 22:03:37 -0300

    separando mas plantillas

---
+++ static/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', function () {
+    // Obtiene el campo de entrada de texto por su ID
+    var input = document.getElementById('quepasa');
+
+    // Posiciona el cursor en el campo de entrada de texto
+    input.focus();
+});
